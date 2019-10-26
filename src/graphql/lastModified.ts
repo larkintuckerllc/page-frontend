641974fdc0834913d007edfd8ab97ec0fe173d2e
John Tucker <jtucker@appirio.com>
2019-10-26 08:30:22 -0400

    checkpoint

--- src/graphql/lastModified.ts
+++
@@ -1,5 +0,0 @@
-let booksLastModified = 0;
-export const booksGetLastModified = (): number => booksLastModified;
-export const booksSetLastModified = (lastModified: number): void => {
-  booksLastModified = lastModified;
-};
